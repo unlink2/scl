8cd6daaf82be94a1d783146c3c54cec04f159441
Lukas Krickl <lukas@krickl.dev>
2022-10-01 21:12:32 +0200

    Fixed alloc

--- src/sclalloc.c
+++ src/sclalloc.c
@@ -1,6 +1,6 @@
 #include "sclalloc.h"
 
-SclAlloc alloc_init(SclMalloc malloc, SclFree free) {
+SclAlloc scl_alloc_init(SclMalloc malloc, SclFree free) {
   SclAlloc alloc = {malloc, free};
 
   return alloc;
