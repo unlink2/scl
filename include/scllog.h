17f7b256de5f76bf352ee56051325c7f81828214
Lukas Krickl <lukas@krickl.dev>
2022-10-16 20:23:58 +0200

    Added ini default handlers

--- include/scllog.h
+++ include/scllog.h
@@ -4,6 +4,10 @@
 #define str_fmt "%.*s"
 #define str_out(str) str.len, str.raw
 
+#define sclerr stderr 
+#define sclout stdout 
+#define sclin stdin
+
 typedef enum LogLevel { OUTPUT, ERROR, INFO, DEBUG, LOG_LEVEL_LEN } LogLevel;
 
 void scl_log_set_level(LogLevel level);
